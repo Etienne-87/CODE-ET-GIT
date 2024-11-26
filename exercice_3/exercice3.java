b9dc5868c1317597fa92ed3a18cae117873a98b1
Etienne <anglerae@3il.fr>
2024-11-26 14:57:01 +0100

    Correction de la propreté du code de l'exercice 3

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -2,21 +2,43 @@ public class StockManager {
 
     // gere le stock
     public void gererStock(String typeOperation, String produit, int quantite, int stock) {
-        // Ajout au stock
-        if (typeOperation.equals("ajout")) {
-            stock += quantite;
-            System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
-        } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
-                System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
-            }
-        // Si l'action est inconnu
-        } else {
+        if(typeOperation != retrait || typeOperation != ajout){
             System.out.println("Opération inconnue.");
+                return;
+            }
+            else if (typeOperation.equals("ajout"){
+                ajoutStock(produit, quantite, stock);
+                System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
+            }
+            else if (typeOperation.equals("retrait"){
+                retraitStock(produit, quantite, stock);
+            }
+        }
+    /**
+     * Ajout ou retrait de stock
+     * 
+     * @param produit
+     * @param quantite
+     * @param stock
+     */
+    private void ajoutStock(String produit, int quantite, int stock) {
+        stock += quantite;
+    }
+
+    /**
+     * Retrait de stock
+     * 
+     * @param produit
+     * @param quantite
+     * @param stock
+     */
+    retraitStock(String produit, int quantite, int stock) {
+        if (stock >= quantite) {
+            stock -= quantite;
+            System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
+        } else {
+        System.out.println("Stock insuffisant pour le produit : " + produit);
         }
     }
+    
 }
